c613c1c127909d8aff61eb4e7af12708f5e99998
Francis Decena <francisdecena@franciss-mbp.lan>
2020-10-30 22:01:08 +0800

    Commit changes

---
+++ ZumbaApp/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // This will run the spinner when the user click the save button in UserSettingIndex
+    $("#btnUpdateUser").click(function () {
+        // add spinner to button
+        $(this).html(
+            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+        );
+    });
+
+    // This will run the spinner when the user click the register button in RegisterPage
+    $("#btnRegisterUser").click(function () {
+        // add spinner to button
+        $(this).html(
+            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+        );
+    });
+});
